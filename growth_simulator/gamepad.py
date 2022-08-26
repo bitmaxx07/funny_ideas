34181d945b97aad52826c8808d481584e415b760
Xun Ma <bitmaxx07@gmail.com>
2022-08-26 18:20:22 -0500

    added new folder and files

---
+++ growth_simulator/gamepad.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+pygame.init()
+
+screen = pygame.display.set_mode([500, 500])
+
+running = True
+
+# set game running
+
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
