2fa461e95e8f3dccf93f6615ebd1a89f278753ce
Xun Ma <bitmaxx07@gmail.com>
2022-08-29 11:51:49 -0500

    implemented the basic function

--- growth_simulator/gamepad.py
+++ growth_simulator/gamepad.py
@@ -9,14 +9,45 @@ from pygame.locals import (
     QUIT,
 )
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-running = True
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -2)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 2)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-2, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(2, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
 
 # set game running
 
+player = Player()
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -24,3 +55,8 @@ while running:
                 running = False
         elif event.type == QUIT:
             running = False
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+    screen.fill((0, 0, 0))
+    screen.blit(player.surf, player.rect)
+    pygame.display.flip()
