2b4fb7093022ed836760fdcfa571f0dc0130635b
Xun Ma <bitmaxx07@gmail.com>
2022-08-29 20:09:27 -0500

    added more functions

--- growth_simulator/gamepad.py
+++ growth_simulator/gamepad.py
@@ -1,4 +1,7 @@
 import pygame
+import random
+
+
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -11,10 +14,27 @@ from pygame.locals import (
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+score = 0
+life = 5
+
+
+def get_life():
+    return str(life)
+
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Test_game")
+font = pygame.font.Font("freesansbold.ttf", 32)
+msg = "Life: " + get_life()
+text = font.render(msg, True, (0, 255, 0), (0, 0, 128))
+text_rect = text.get_rect()
+text_rect.center = (80, 40)
+
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
 
 
 class Player(pygame.sprite.Sprite):
@@ -26,13 +46,13 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -2)
+            self.rect.move_ip(0, -3)
         if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 2)
+            self.rect.move_ip(0, 3)
         if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-2, 0)
+            self.rect.move_ip(-3, 0)
         if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(2, 0)
+            self.rect.move_ip(3, 0)
 
         if self.rect.left < 0:
             self.rect.left = 0
@@ -44,19 +64,61 @@ class Player(pygame.sprite.Sprite):
             self.rect.bottom = SCREEN_HEIGHT
 
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 0))
+        self.rect = self.surf.get_rect(
+            center=(random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                    random.randint(0, SCREEN_HEIGHT), )
+        )
+        self.speed = random.randint(5, 20)
+
+    def update(self):
+        self.rect.move_ip(-self.speed / 8, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+
 # set game running
 
 player = Player()
+
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
+
         elif event.type == QUIT:
             running = False
+
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
     pressed_keys = pygame.key.get_pressed()
     player.update(pressed_keys)
+    enemies.update()
+
     screen.fill((0, 0, 0))
-    screen.blit(player.surf, player.rect)
+    screen.blit(text, text_rect)
+    # screen.blit(player.surf, player.rect)
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    if pygame.sprite.spritecollideany(player, enemies):
+        life = life - 1
+
+    if life == 0:
+        player.kill()
+        running = False
+
     pygame.display.flip()
